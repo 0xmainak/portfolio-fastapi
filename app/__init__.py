bc408e5896a5780f7cace3d436aa8cba561b73cb
0xmainak <mainakpanda30@gmail.com>
2025-09-02 09:03:54 +0530

    Added CORS middleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from .routes import router, github_router, spotify_router, contributions_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def init_app():
-    app = FastAPI()
+    app = FastAPI(
+        docs_url=None,
+        redoc_url=None,
+        openapi_url=None,
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[os.getenv("FRONTEND_URL"), "localhost"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    
     app.include_router(router=router)
     app.include_router(router=spotify_router, prefix="/spotify")
     app.include_router(router=contributions_router, prefix="/github")
